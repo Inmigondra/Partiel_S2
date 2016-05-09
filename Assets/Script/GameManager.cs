b36d2a818a888459b967f81a827401c335426795
Inmigondra <inmigondra@live.fr>
2016-05-09 21:38:18 +0200

    save

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,16 +1,55 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameManager : MonoBehaviour {
+    public static GameManager control;
+    GameObject selectedAvatar; //avatar used in game
 
-	// Use this for initialization
-	void Start () {
-	
+    public Texture[] avatarImage = new Texture[3];
+    int highscore;
+    int nextLevel;
+    bool firstOptional;
+    bool secondOptional;
+
+
+    // Use this for initialization
+    void Awake () {
+        if (control == null) {
+            DontDestroyOnLoad(this.gameObject);
+            control = this;
+        }else if (control != null){
+            Destroy(gameObject);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    public void LevelLoad (int level) {
+        SceneManager.LoadScene(level);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+    void Load () {
+
+    }
+    void Save () {
+        BinaryFormatter bf = new BinaryFormatter() ;
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+        GameStat gt = new GameStat();
+
+        bf.Serialize(file, gt);
+        file.Close();
+    }
+
+    [Serializable]
+    class GameStat {
+
+    }
 }
