a739eed699a2eef51c461a39535eaac087903335
Inmigondra <inmigondra@live.fr>
2016-05-10 21:06:10 +0200

    change

---
+++ Assets/Script/Intro.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Intro : MonoBehaviour {
+
+	public void LoadLevel(int level){
+        SceneManager.LoadScene(level);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
     public int speed;
     public GameObject[] projectile= new GameObject[3];
+    GameObject gameManager;
+    GameManager GM;
     Vector2 mousePosition;
     static int avatar;
 	// Use this for initialization
 	void Start () {
-	
+        gameManager = GameObject.Find("GameManager");
+        GM = gameManager.GetComponent<GameManager>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour {
         //if the viewportposition is out of (0,0) or (1,1)
         if (playerToCamera.x > 1 || playerToCamera.x < 0 || playerToCamera.y > 1 || playerToCamera.y < 0) {
             Debug.Log("gameover");
+            GM.LevelLoad(3);
         }
 
       
